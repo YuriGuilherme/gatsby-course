67dba57edcae3a2c56cc65aa7837f621c050863b
Yuri Guilherme <yurigm97@hotmail.com>
2019-09-22 20:26:57 -0300

    feat(social-links): add social links content

---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,25 @@
+const links = [
+    {
+        label: 'Github',
+        url: 'https://github.com/YuriGuilherme',
+    },
+    {
+        label: 'Twitter',
+        url: 'https://twitter.com/yurigm97',
+    },
+    {
+        label: 'Youtube',
+        url: '',
+    },
+    {
+        label: 'Instagram',
+        url: 'https://www.instagram.com/yurigm97',
+    },
+    {
+        label: 'Unsplash',
+        url: 'https://unsplash.com/@yuriguilherme',
+    },
+]
+
+
+export default links
